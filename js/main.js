18b0302c8378b0b64c1f5000b38e6d901604a2dc
johnchourajr <hello@objectsubject.com>
2016-05-06 10:07:58 -0700

    added smoothstate

--- js/main.js
+++ js/main.js
@@ -1,4 +1,46 @@
 // ---------------
+// smoothState Init
+// ---------------
+
+$(function(){
+  'use strict';
+  var $page = $('#doc'),
+      options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 2,
+        onStart: {
+          duration: 250, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $container.addClass('is-exiting');
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onReady: {
+          duration: 0,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $container.removeClass('is-exiting');
+            // Inject the new content
+            $container.html($newContent);
+          }
+        }
+      },
+      smoothState = $page.smoothState(options).data('smoothState');
+});
+
+
+// ---------------
+// Ext. Link Click
+// ---------------
+$(document.links).filter(function() {
+  return this.hostname != window.location.hostname;
+}).attr('target', '_blank');
+
+
+// ---------------
 // Header Click
 // ---------------
 $('#designer').on('click', function(evt) {
