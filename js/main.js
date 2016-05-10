aa6b31abb529538fc42c942b49b7a78734378abd
johnchourajr <hello@objectsubject.com>
2016-05-09 23:17:37 -0700

    mods to styles

--- js/main.js
+++ js/main.js
@@ -3,9 +3,38 @@
 // smoothState Init
 // ---------------
 
-$(function() {
+// $(function() {
+//   'use strict';
+//   $('.doc').smoothState({ debug: true, prefetch: true, cacheLength: 2, scroll: true, });
+// });
+
+$(function(){
   'use strict';
-  $('.doc').smoothState({ debug: true, prefetch: true, cacheLength: 2, scroll: true, });
+  var $page = $('#doc'),
+      options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 2,
+        onStart: {
+          duration: 250, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $container.addClass('is-exiting');
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onReady: {
+          duration: 0,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $container.removeClass('is-exiting');
+            // Inject the new content
+            $container.html($newContent);
+          }
+        }
+      },
+      smoothState = $page.smoothState(options).data('smoothState');
 });
 
 // ---------------
