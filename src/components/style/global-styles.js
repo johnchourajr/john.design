d450fb3a23dc5ea328569ce4ca896f3dccfa75bc
John Choura <jchoura@godaddy.com>
2021-01-26 20:57:48 -0800

    fixing runtime issue

--- src/components/style/global-styles.js
+++ src/components/style/global-styles.js
@@ -2,23 +2,21 @@ import styled from "styled-components";
 // import { clampBuilder } from "../../functions/util";
 
 export function clampBuilder(minFontSize, maxFontSize) {
-	if (typeof document !== `undefined`) {
-		// const root = document.querySelector("html");
-		// const pixelsPerRem = Number(getComputedStyle(root).fontSize.slice(0, -2));
+	// const root = document.querySelector("html");
+	// const pixelsPerRem = Number(getComputedStyle(root).fontSize.slice(0, -2));
 
-		// console.log({ root, pixelsPerRem });
+	// console.log({ root, pixelsPerRem });
 
-		const pixelsPerRem = 16;
-		const minWidth = 375 / pixelsPerRem;
-		const maxWidth = 1440 / pixelsPerRem;
+	const pixelsPerRem = 16;
+	const minWidth = 375 / pixelsPerRem;
+	const maxWidth = 1440 / pixelsPerRem;
 
-		const slope = (maxFontSize - minFontSize) / (maxWidth - minWidth);
-		const yAxisIntersection = -minWidth * slope + minFontSize;
+	const slope = (maxFontSize - minFontSize) / (maxWidth - minWidth);
+	const yAxisIntersection = -minWidth * slope + minFontSize;
 
-		return `clamp( ${minFontSize}rem, ${yAxisIntersection}rem + ${
-			slope * 100
-		}vw, ${maxFontSize}rem )`;
-	}
+	return `clamp( ${minFontSize}rem, ${yAxisIntersection}rem + ${
+		slope * 100
+	}vw, ${maxFontSize}rem )`;
 }
 
 export const PageWrapper = styled.div`
