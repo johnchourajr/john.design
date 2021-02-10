225b9a6b1e6983b7592ba14c85101622052f156a
John Choura <jchoura@godaddy.com>
2021-02-09 22:37:26 -0800

    removing gatsby-browser

--- src/components/globals/head.js
+++ src/components/globals/head.js
@@ -1,13 +1,20 @@
-import React from "react";
-import Helmet from "react-helmet";
+import React from 'react';
+import Helmet from 'react-helmet';
 
 function Head({ title }) {
-	return (
-		<Helmet>
-			<title>{title}</title>
-			<meta name="description" content="Helmet application" />
-		</Helmet>
-	);
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content="Helmet application" />
+      <meta name="robots" content="noindex" />
+      <meta name="googlebot" content="noindex" />
+      <meta name="googlebot-news" content="nosnippet" />
+      <meta
+        name="viewport"
+        content="width=device-width initial-scale=1 shrink-to-fit=no"
+      />
+    </Helmet>
+  );
 }
 
 export default Head;
