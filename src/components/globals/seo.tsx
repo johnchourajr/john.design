97867d56651a2a341bbb0b7206357328cafc3874
johnchourajr <hi@john.design>
2022-06-27 00:36:09 -0700

    adding favicon

--- src/components/globals/seo.tsx
+++ src/components/globals/seo.tsx
@@ -10,7 +10,11 @@ interface SEOProps {
   image?: string;
   title?: string;
 }
-function SEO({ description, image, title = "Studio" }: SEOProps) {
+function SEO({
+  description = config.description,
+  image,
+  title = "Studio",
+}: SEOProps) {
   const siteTitle = config.title;
 
   return (
@@ -27,6 +31,27 @@ function SEO({ description, image, title = "Studio" }: SEOProps) {
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       {image && <meta property="twitter:iamge" content={image} />}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="icon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="icon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="icon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="icon/site.webmanifest" />
+      <link rel="mask-icon" href="icon/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 }
