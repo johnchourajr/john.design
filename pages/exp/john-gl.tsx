ea902f856c3c17a1eb2a8019c98f7ef63fc29e84
johnchourajr <hi@john.design>
2022-12-04 23:31:23 -0800

    adding post processing

--- pages/exp/john-gl.tsx
+++ pages/exp/john-gl.tsx
@@ -1,9 +1,10 @@
 import * as THREE from "three";
-import React, { Suspense, useEffect, useRef, useState } from "react";
+import React, { FC, Suspense, useRef, useState } from "react";
 import { Canvas, useLoader, useFrame, useThree } from "@react-three/fiber";
+import { EffectComposer, Noise } from "@react-three/postprocessing";
 
 import InlineLink from "../../components/InlineLink";
-import { useDevicePixelRatio, useWindowSize } from "../../utils/hooks";
+import { useDevicePixelRatio } from "../../utils/hooks";
 import { motion } from "framer-motion-3d";
 import {
   getSettingValue,
@@ -19,90 +20,94 @@ function Light({ type, light, hide, ...rest }: any) {
   } else return null;
 }
 
+export const Effect: FC = () => {
+  return (
+    <EffectComposer>
+      <Noise opacity={0.035} />
+    </EffectComposer>
+  );
+};
+
 function Mesh({ settings }: any) {
   const ref = useRef<THREE.Mesh>();
-  const [me, memap] = useLoader(THREE.TextureLoader, ["/me.png", "/memap.png"]);
+  const [me, memap] = useLoader(THREE.TextureLoader, [
+    "/me-alpha.png",
+    "/me-map.png",
+  ]);
 
   const light_1 = getSettingValue(settings, "Light 1", true);
   const light_2 = getSettingValue(settings, "Light 2", true);
   const light_3 = getSettingValue(settings, "Light 3", true);
-  const metalness = getSettingValue(settings, "Metalness", 0.01);
+  const metalness = getSettingValue(settings, "Metalness", 0.5);
 
   // use mouse position to rotate the mesh
   const { mouse } = useThree();
 
   // use mouse position to move lights
-  const [x, setX] = useState(1);
-  const [y, setY] = useState(1);
+  const [x, setX] = useState(3);
+  const [y, setY] = useState(3);
 
   useFrame(() => {
-    setX(mouse.x);
-    setY(mouse.y);
+    setX(mouse.x * 1);
+    setY(mouse.y * 1);
   });
 
   const light1 = {
     x: Math.sin(-x),
-    y: Math.cos(-x + y),
-    z: 0.2,
+    y: Math.cos(-x + y) - 0.3,
+    z: 0,
   };
 
   const light2 = {
     x: Math.sin(x),
-    y: Math.cos(x + y),
-    z: 0.2,
+    y: Math.cos(x + y) - 0.3,
+    z: 0,
   };
 
   const light3 = {
-    x: 0,
-    y: y - 2,
+    x: 1,
+    y: Math.sin(x + y) - 2,
     z: 1,
   };
 
-  // console.log({
-  //   light2Y: light2.y,
-  //   light2X: light2.x,
-  // });
+  const sharedProps = {
+    intensity: 10,
+    distance: 100,
+    penumbra: 1,
+    angle: 1,
+    castShadow: true,
+    transition: { ease: [0.16, 1, 0.3, 1] },
+  };
 
   return (
     <mesh ref={ref as any} scale={1}>
       <Light
         type={"spotLight"}
         light={light1 as any}
-        intensity={10}
         color={0x0000ff}
-        transition={{ ease: [0.16, 1, 0.3, 1] }}
-        angle={0.5}
-        distance={100}
-        penumbra={1}
-        castShadow
         hide={!light_1}
+        {...sharedProps}
       />
       <Light
         type={"spotLight"}
         light={light2 as any}
-        intensity={10}
         color={0xff0000}
-        transition={{ ease: [0.16, 1, 0.3, 1] }}
-        angle={0.5}
-        distance={100}
-        penumbra={1}
-        castShadow
         hide={!light_2}
+        {...sharedProps}
       />
       <Light
         type={"pointLight"}
         light={light3 as any}
-        intensity={0.8}
+        intensity={2.2}
         color={0xffffff}
         hide={!light_3}
       />
-      <motion.planeGeometry attach="geometry" args={[1, 1, 1]} />
+      <motion.planeGeometry attach="geometry" args={[1, 1]} />
       <meshStandardMaterial
         metalness={metalness}
         roughness={0}
         normalMap={memap}
         map={me}
-        transparent
       />
     </mesh>
   );
@@ -125,9 +130,14 @@ const SETTINGS = [
     value: true,
   },
   {
+    name: "Noise",
+    type: "Boolean",
+    value: true,
+  },
+  {
     name: "Metalness",
     type: "Slider",
-    value: 0.01,
+    value: 0.5,
     step: 0.01,
     min: 0,
     max: 0.5,
@@ -135,30 +145,22 @@ const SETTINGS = [
 ];
 
 export function JohnGLCanvas({ settings }: any) {
-  const { width = 0, height = 0 } = useWindowSize();
   const devicePixelRatio = useDevicePixelRatio();
-  const [size, setSize] = useState({ width: 0, height: 0 });
 
-  useEffect(() => {
-    setSize({ width, height });
-  }, [width, height]);
+  const noise = getSettingValue(settings, "Noise", false);
 
   return (
     <div className="absolute inset-0 z-[0]">
       <Canvas
-        style={
-          {
-            // width: size.width,
-            // height: size.width,
-            // maxHeight: "100vh",
-            // paddingTop: "6%",
-          }
-        }
+        style={{
+          width: "100vw",
+          height: "100vw",
+        }}
         dpr={devicePixelRatio || 3}
         camera={{
           position: [0, 0, 2],
+          aspect: 1,
           fov: 25,
-          aspect: width / height,
           near: 0.2,
           far: 100,
         }}
@@ -166,6 +168,7 @@ export function JohnGLCanvas({ settings }: any) {
         <Suspense fallback={null}>
           <Mesh settings={settings} />
         </Suspense>
+        {noise && <Effect />}
       </Canvas>
     </div>
   );
