29689549ee83f552d301486ea13d85cf23330ec5
johnchourajr <hi@john.design>
2024-12-30 00:11:16 -0800

    update

--- components/globals/LayoutInner.tsx
+++ components/globals/LayoutInner.tsx
@@ -2,14 +2,14 @@
 
 import clsx from 'clsx';
 import { useSearchParams } from 'next/navigation';
-import { Suspense, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDrawing } from '../experimental/Drawing';
 
 export type LayoutInnerProps = {
   children: React.ReactNode;
 };
 
-function LayoutInnerLoader({ children }: LayoutInnerProps) {
+export function LayoutInner({ children }: LayoutInnerProps) {
   const searchParams = useSearchParams();
   const isIframe = searchParams.get('iframe') !== null;
   const { setEnableDrawing } = useDrawing();
@@ -24,13 +24,5 @@ function LayoutInnerLoader({ children }: LayoutInnerProps) {
     <main className={clsx('relative z-10', !isIframe && 'min-h-[100vh] pt-14')}>
       {children}
     </main>
-  );
-}
-
-export function LayoutInner({ children }: LayoutInnerProps) {
-  return (
-    <Suspense>
-      <LayoutInnerLoader>{children}</LayoutInnerLoader>
-    </Suspense>
   );
 }
